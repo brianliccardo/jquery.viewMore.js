ff2944269f9cfd6ba8025986287b767275b77ef3
Brian Liccardo <brianliccardo@gmail.com>
2014-12-02 09:34:23 -0500

    added wrapper so toggler doesnt obscure text

--- jquery.viewMore.js
+++ jquery.viewMore.js
@@ -1,11 +1,12 @@
 /*!
- * viewMore v0.1.0
+ * viewMore v0.2.0
  */
 ;(function($) {
 	var instance = 1;
 	var viewMore = function($container, options, idx) {
 		var vmId = 'viewMore'+idx;
-		var dh = $container.height();//.offset().top;
+		var wrapperId = 'viewMoreWrap'+idx;
+		var dh = $container.height();
 		var lh = parseFloat($container.css('line-height').replace('px', ''));
 		var lines = dh / lh;
 
@@ -13,39 +14,37 @@
 			var visHeight = (lh * options.maxLines);
 			var origHeight = $container.height();
 			$container.css('height', visHeight).css('overflow','hidden').css('margin-bottom',lh);
-			var top = $container.offset().top + dh + lh;
-
-			$('body').append('<span id="'+vmId+'" class="viewMore" style="background-color: '+options.bgColor+'; cursor: pointer;" data-state="closed">'+options.moreText+'</span>');
-			var vmWidth = $('#'+vmId).width();
-			var vmHeight = $('#'+vmId).height();
-			var $viewMore = $('#'+vmId).detach()
-
-			var right = $container.offset().left + $container.width() - vmWidth;
-
-			$container.css('position', 'relative');
 			
-			var top = visHeight - vmHeight;
-			$viewMore.css('position','absolute').css('top',top+'px').css('right','0');
+			// wrap container in div
+			$container.wrap('<div id="'+wrapperId+'"></div>');
+			var $wrapper = $('#'+wrapperId);
+			
+			// add toggler to wrapper
+			$wrapper.append('<span id="'+vmId+'" class="viewMoreToggle" style="cursor: pointer;" data-state="closed">'+options.moreText+'</span>');
+			
+			$viewMore = $('#'+vmId);
+			
+			var top = visHeight;
 			$viewMore.click(function(e){
 				if ($(this).data('state') == 'closed') {
-					$container.css('height','auto');
+					
+					var curHeight = $container.height();
+					$container.css('height', 'auto');
+					var autoHeight = $container.height();
+					$container.height(curHeight)
+					
+					$container.animate({height:autoHeight},200);
+					
 					$(this)
 						.data('state', 'open')
-						.css('top', 'auto')
-						.css('position', '')
-						.css('float', 'right')
 						.html(options.lessText);
 				} else {
-					$container.height(visHeight);
+					$container.animate({height:visHeight},200);
 					$(this)
 						.data('state', 'closed')
-						.css('top', top)
-						.css('float', 'auto')
-						.css('position', 'absolute')
 						.html(options.moreText);
 				}
 			});
-			$container.append($viewMore)
 		}
 	}
 
@@ -55,8 +54,7 @@
 		var defaults = {
 			moreText	: 'View More',
 			lessText	: 'View Less',
-			maxLines	: 2,
-			bgColor		: '#FFF'
+			maxLines	: 2
 		};
 		
 		var options =  $.extend(true, defaults, options);
